891499c65b992efe64374d2c47b00958fe97124e
steffen.simmons <adeptusforgeceo@gmail.com>
2025-06-11 02:18:07 -0700

    Fix

--- website/src/pages/BasicPageTemplate.tsx
+++ website/src/pages/BasicPageTemplate.tsx
@@ -1,7 +1,7 @@
 import HeaderBar from "../components/HeaderBar" 
 import './BasicPageTemplate.css'
 
-export default function PortfolioPage({title=""})
+export default function PortfolioPage()
 {
   return <div className="PageEntry">
     <div className="BackgroundImage"></div>
